46e5d425cbdee9ddaf1792e161d2e7df3e6a9ab2
Amauri <amaurirg@terra.com.br>
2021-02-17 18:42:05 -0200

    checkout

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from checkout.views import create_cartitem
+from checkout.views import create_cartitem, cart_item
 
 
 app_name = 'checkout'
 urlpatterns = [
     path('carrinho/adicionar/<slug:slug>', create_cartitem, name='create_cartitem'),
-    # path('carrinho/', cart_item, name='cart_item')
+    path('carrinho/', cart_item, name='cart_item')
 ]
